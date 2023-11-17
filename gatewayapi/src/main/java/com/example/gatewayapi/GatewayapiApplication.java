3d27aef8d4799976a3e643ff02846ef81b645afc
mechanist1 <mlaouhiilyess@gmail.com>
2023-11-17 23:02:00 +0100

    created the gateway-api but still facing the scrf problem

--- gatewayapi/src/main/java/com/example/gatewayapi/GatewayapiApplication.java
+++ gatewayapi/src/main/java/com/example/gatewayapi/GatewayapiApplication.java
@@ -2,12 +2,17 @@ package com.example.gatewayapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 
 @SpringBootApplication
 public class GatewayapiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayapiApplication.class, args);
+        System.out.println("gateway_api is working properly");
     }
+
+
+
 
 }
