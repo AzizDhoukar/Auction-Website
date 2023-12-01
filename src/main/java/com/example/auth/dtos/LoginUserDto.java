d6fdeeeb61a93063dc9767c1364db13d0455b7ad
baha ouni <bahaouni1@gmail.com>
2023-12-01 21:10:13 +0100

    first commit

---
+++ src/main/java/com/example/auth/dtos/LoginUserDto.java
@@ -0,0 +1,16 @@
+package com.example.auth.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class LoginUserDto {
+    private String email;
+
+    private String password;
+
+    // getters and setters here...
+}
